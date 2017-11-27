ae492046d8db2d32de46637886aebf3c76ef0ab4
ericgrosse <ericgrosse1@gmail.com>
2017-11-27 08:59:02 -0500

    Re-arranged the ImageCards, updated descriptions

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -42,10 +42,38 @@ class Home extends Component {
             </div>
 
             <ImageCard
+              href='http://www.webdevsanity.com/'
+              header='Web Dev Sanity'
+              description='My personal programming blog, with an emphasis on making web development more accessible to new developers.'
+              imagePath={require('images/web-dev-sanity.png')}
+            />
+
+            <ImageCard
               href='http://ketogenicookbook.com/'
               header='Ketogenic Cookbook'
               description='A sample website for making recipes that adhere to a ketogenic diet.'
               imagePath={require('images/ketogenic-cookbook-1.png')}
+            />
+
+            <ImageCard
+              href='http://githubsampleapi.com'
+              header='Github Sample API'
+              description={'An Angular application that uses Github\'s APIs to conveniently search through Github repositories, as well as checking for contributors, languages, stargazers, subscribers, etc.'}
+              imagePath={require('images/github-sample-api.png')}
+            />
+
+            <ImageCard
+              href='https://book-in-a-box.herokuapp.com/'
+              header='Book In A Box'
+              description='A site designed for potential authors who want professional help publishing their first book. Currently the homepage, About Us, Our Authors and Blog page are built.'
+              imagePath={require('images/book-in-a-box.png')}
+            />
+
+            <ImageCard
+              href='https://toptal-user-profile.herokuapp.com/'
+              header='Toptal User Profile'
+              description='A sample site for developers to upload their profile and resume in order to attract clients. Each square in the profile allows the user to add content.'
+              imagePath={require('images/toptal-user-profile.png')}
             />
 
             <ImageCard
@@ -58,53 +86,23 @@ class Home extends Component {
             <ImageCard
               href='http://www.feandrarealestate.com/'
               header='Feandra Real Estate'
-              description=''
+              description='A sample site that displays real estate listings.'
               imagePath={require('images/feandra-real-estate.png')}
-            />
-
-            <ImageCard
-              href='http://www.searchifyprofiles.com/'
-              header='Searchify Profiles'
-              description=''
-              imagePath={require('images/searchify-profiles.png')}
-            />
-
-            <ImageCard
-              href='http://www.webdevsanity.com/'
-              header='Web Dev Sanity'
-              description='My personal programming blog, with an emphasis on making web development more accessible to new developers.'
-              imagePath={require('images/web-dev-sanity.png')}
             />
 
             <ImageCard
               href='http://avertourism.com/'
               header='Aver Tourism'
-              description=''
+              description='A sample tourism site featuring an image carousel, on-scroll image animations and styled using Bootstrap.'
               imagePath={require('images/aver-tourism.png')}
             />
 
             <ImageCard
-              href='http://githubsampleapi.com'
-              header='Github Sample API'
-              description=''
-              imagePath={require('images/github-sample-api.png')}
+              href='http://www.searchifyprofiles.com/'
+              header='Searchify Profiles'
+              description='An experimental site that uses a fixed background image with angled tiles to fit the content. The header also features a complex, customized CSS animation.'
+              imagePath={require('images/searchify-profiles.png')}
             />
-
-            <ImageCard
-              href='https://book-in-a-box.herokuapp.com/'
-              header='Book In A Box'
-              description=''
-              imagePath={require('images/book-in-a-box.png')}
-            />
-
-            <ImageCard
-              href='https://toptal-user-profile.herokuapp.com/'
-              header='Toptal User Profile'
-              description=''
-              imagePath={require('images/toptal-user-profile.png')}
-            />
-            
-
           </div>
         </section>
       </div>
