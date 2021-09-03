be7c931104ed17e76620957f732cbaf5773ba760
Eric Grosse <=>
2021-09-03 09:11:25 -0400

    Updated years experience

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -20,7 +20,7 @@ class AboutMe extends Component {
           <img src={require('images/avatar.jpg')}/>
 
           <div className="info">
-            <p>Hi, I'm Eric Grossé, a frontend web developer from Montreal, Canada. I've been working in web development for over two years, ranging from freelance work to employment from startups and large corporations.</p>
+            <p>Hi, I'm Eric Grossé, a frontend web developer from Montreal, Canada. I've been working in web development for over three and a half years, ranging from freelance work to employment from startups and large corporations.</p>
             <p>I am passionate about coding because I love using my intellect to create value (such as new social media platforms) out of mere keystrokes.</p>
             <p>If you are interested in my services, feel free to contact me by <a href="mailto:ericgrosse1@gmail.com">email</a>, or via social media.</p>
 
