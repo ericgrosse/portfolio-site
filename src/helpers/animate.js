0aa956fe9e481ed9a4b883ca9e1322c6ca9c0a04
Eric Grossé <ericgrosse1@gmail.com>
2023-03-13 12:08:23 -0400

    added back react code

---
+++ src/helpers/animate.js
@@ -0,0 +1,49 @@
+import $ from 'jquery';
+
+function animate() {
+  /* Animate image when scrolling to it */
+
+  let $animatedIcons = $('.section-6'); // Add classnames here
+  let $window = $(window);
+  let lastScrollTop = 0;
+
+  function checkIfInView() {
+    let windowHeight = $window.height();
+    let windowTopPosition = $window.scrollTop();
+    let windowBottomPosition = windowTopPosition + windowHeight;
+
+    /* Show the navbar if scrolling upwards */
+
+    //if(windowTopPosition > 62) {
+      if(windowTopPosition < lastScrollTop) {
+        $('.Header, .MobileHeader').removeClass('scroll-down').addClass('scroll-up');
+      }
+      else {
+        $('.Header, .MobileHeader').removeClass('scroll-up').addClass('scroll-down');
+      }     
+    //}
+    /*else {
+      $('.Header').removeClass('scoll-down').removeClass('scroll-up');
+    }*/
+
+    lastScrollTop = windowTopPosition;
+  }
+
+  $window.on('scroll', checkIfInView);
+
+  /* Scroll to anchor text */
+  let $html_body = $('html, body');
+
+  $('a[href^=\\#]').click(function(e) {
+   if ($(window).width() < 768) {
+     return;
+   }
+
+    //e.preventDefault();
+    let dest = $(this).attr('href');
+    let scrollTop = dest === '#home' ? 0 : $(dest).offset().top;
+    $html_body.animate({ scrollTop: scrollTop }, 'slow');
+  });
+}
+
+export default animate;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+ <div class="Home">
+  <h1>egportfolio</h1>
+ </div>
+
+<!-- import React, { Component } from 'react';
+import './Home.scss';
+import Header from 'components/Header'
+import AboutMe from 'components/AboutMe'
+import MyWork from 'components/MyWork'
+import MyExperience from 'components/MyExperience'
+import MyExpertise from 'components/MyExpertise'
+import Footer from 'components/Footer'
+import onScroll from 'helpers/onScroll';
+import animate from 'helpers/animate';
+import 'animate.css/animate.min.css'
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    onScroll()
+    animate()
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        <Header/>
+        <AboutMe/>
+        <MyWork/>
+        <MyExperience/>
+        <MyExpertise/>
+        <Footer/>
+      </div>
+    )
+  }
+}
+
+export default Home;
+ -->
+
+<!-- import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.scss';
+import Home from './Home';
+import * as serviceWorker from './serviceWorker';
+import 'fonts/Pieta/Webfonts/MyFontsWebfontsKit.css';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister(); -->
